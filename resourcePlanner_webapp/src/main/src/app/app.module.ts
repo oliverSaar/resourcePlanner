608dd89f2dda89aeb8a10316ff4e207ba2bb360c
Oliver Saar <oli.saar123@gmail.com>
2023-02-12 14:45:50 +0100

    modules

--- resourcePlanner_webapp/src/main/src/app/app.module.ts
+++ resourcePlanner_webapp/src/main/src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CalendarComponent} from "./calendar/calendar.component";
+import {EmployeesComponent} from "./employees/employees.component";
+import {ProjectsComponent} from "./projects/projects.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    EmployeesComponent,
+    ProjectsComponent
+
   ],
   imports: [
     BrowserModule,
@@ -15,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
