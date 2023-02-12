88e2f9e5ae8441ee5f1b9ff17fe7654d4cd5c8a9
Oliver Saar <oli.saar123@gmail.com>
2023-02-12 14:46:44 +0100

    Gruppen

---
+++ resourcePlanner_modell/src/main/java/de/edeka/sw/webapps/resourcePlanner/model/constants/Gruppen.java
@@ -0,0 +1,10 @@
+package de.edeka.sw.webapps.resourcePlanner.model.constants;
+
+public class Gruppen {
+
+    public static final String USER_GH = "User_GH";
+    public static final String USER_GH_IT = "User_GH_IT";
+
+    public static final String ADMINS_GH = "User_GH_ResourcePlanner_Admins";
+
+}
