86f293e020590399e3347f8e2e7c5fd83aaefbb3
Oliver Saar <oli.saar123@gmail.com>
2023-02-19 20:26:03 +0100

    running

--- resourcePlanner_webapp/src/main/webapp/src/app/app.module.ts
+++ resourcePlanner_webapp/src/main/webapp/src/app/app.module.ts
@@ -1,31 +1,26 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app_routing/app-routing.module';
-import {AppComponent} from './root/app.component';
-import {CalendarComponent} from './calendar/calendar.component';
-import {EmployeesComponent} from './employees/employees.component';
-import {ProjectsComponent} from './projects/projects.component';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MaterialModule} from './modules/material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {CalendarModule, DateAdapter} from 'angular-calendar';
-import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
-import {FormsModule} from '@angular/forms';
-import {CalendarBundleModule} from './modules/calendar.module';
-import {FlatpickrModule} from 'angularx-flatpickr';
-import {SafeHtmlPipe} from './pipes/safe-html.pipe';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FormsModule } from '@angular/forms';
+import { CalendarBundleModule } from './modules/calendar.module';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { SafeHtmlPipe } from './pipes/safe-html.pipe';
+import {CalendarComponent} from './calendar/calendar.component';
+import {AppRoutingModule} from './app_routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    EmployeesComponent,
-    ProjectsComponent,
     SafeHtmlPipe
-
   ],
   imports: [
     BrowserModule,
@@ -39,10 +34,9 @@ import {SafeHtmlPipe} from './pipes/safe-html.pipe';
       useFactory: adapterFactory,
     }),
     FlatpickrModule.forRoot(),
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
